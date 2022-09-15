6ce8a1cbec069a78cfc9ac65721107d5b8830d0e
budiyono-dev <budiyono.dev@gmail.com>
2022-09-16 00:07:50 +0700

    enhance function promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+  const allFilm = [...theaterIXX, ...theaterVGC];
+  
+  let countEmosi = 0;
+  
+  allFilm.forEach((film) => {
+    if (film.hasil === emosi) {
+      countEmosi++;
+    }
+  });
+  return countEmosi;
+}
 
 module.exports = {
   promiseOutput,
